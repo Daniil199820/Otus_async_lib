2675270bc0ce07e14b6cb754c93dc38675bdee87
Dannil199820 <dry.ginpost@gmail.com>
2022-11-02 21:57:32 +0300

    example

--- CommandView.h
+++ CommandView.h
@@ -21,38 +21,37 @@ public:
         if(thread_executer.joinable()){
             thread_executer.join();
         }
-        std::cout<<counter_1<<"\n";
-        std::cout<<counter_2<<"\n";
+        std::cout<<"Counter in one thread: "<<counter_1<<"\n";
+        std::cout<<"Counter on another thread: "<<counter_2<<"\n";
+        std::cout<<"Size of common bufer: "<<container.size()<<"\n";
     }
-
+     
     void update(std::deque<Element>& cont) override{
 
         {std::unique_lock<std::mutex> lock(lock_viewer);
-            container.push(cont);
+        container.push(cont);
+        commands_wait.notify_one();
         }
-       // if(counter_1<container.size())counter_1=container.size();
         counter_1 = counter_1 + cont.size();
-       // std::cout<<"update in command view\n";
-        commands_wait.notify_all();
-        
     }
 
     void execute(){
-        
         while(!m_bDone){
             std::unique_lock<std::mutex>locker(lock_viewer);
             commands_wait.wait(locker,[&](){return !container.empty() || m_bDone;});
+            std::cout<<"we in the execute\n";
+            while(!container.empty()){  
             std::deque<Element> temp_c = container.front();
             counter_2 = counter_2 + temp_c.size();
             container.pop();
-            if(!temp_c.empty()){
-                std::cout<<"bulk: "<<temp_c[0]._cmd;
+            std::cout<<"bulk: "<<temp_c[0]._cmd;
             for(size_t i=1;i<temp_c.size();++i){
-                std::cout<<", "<<temp_c[1]._cmd;
+                std::cout<<", "<<temp_c[i]._cmd;
             }
                 std::cout<< '\n';
                 temp_c.clear();
             }
+            
         }
     }
 private:
