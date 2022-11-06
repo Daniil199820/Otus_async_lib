1362ed3e2cdff148751ea50272625129644901a8
Dannil199820 <dry.ginpost@gmail.com>
2022-11-07 00:03:09 +0300

    completed

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -62,6 +62,33 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
   endif()
 endif()
 
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}/home/daniil/cpp/Otus_async_lib/libasync.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/daniil/cpp/Otus_async_lib/libasync.so")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}/home/daniil/cpp/Otus_async_lib/libasync.so"
+         RPATH "")
+  endif()
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/daniil/cpp/Otus_async_lib/libasync.so")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/home/daniil/cpp/Otus_async_lib" TYPE SHARED_LIBRARY FILES "/home/daniil/cpp/Otus_async_lib/build/libasync.so")
+  if(EXISTS "$ENV{DESTDIR}/home/daniil/cpp/Otus_async_lib/libasync.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/daniil/cpp/Otus_async_lib/libasync.so")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/home/daniil/cpp/Otus_async_lib/libasync.so")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/daniil/cpp/Otus_async_lib/build/googletest/cmake_install.cmake")
